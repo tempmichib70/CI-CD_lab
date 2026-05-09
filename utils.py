defe4a639fd3cf45f5a625ff881bc315f5213970
tempmichib70 <elek966@gmail.com>
2026-05-09 16:15:53 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 def add(a: int, b: int) -> int:
     return a+b
 def subtract(a: int, b: int) -> int:
-    return a- b
+    return a-b
 def multiply(a: int, b: int) -> int:
     return a*b
 def divide(a: int, b: int) -> float:
